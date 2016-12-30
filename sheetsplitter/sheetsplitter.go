e2d20352fa7d095108e2fa8a8cb8b78e6c4f61ae
Scott Weiss <sdw35@cornell.edu>
2016-12-30 13:27:44 -0500

    different way of making atlases

---
+++ sheetsplitter/sheetsplitter.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"github.com/Sirupsen/logrus"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"fmt"
+	"encoding/json"
+)
+
+//generates an atlas directly from a single spritesheet
+//diablo-formatted
+
+var j = `
+{
+"attack"
+}
+`
+
+func must(err interface{}) {
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+var rows = []string{
+	"s",
+	"sw",
+	"w",
+	"nw",
+	"n",
+	"ne",
+	"e",
+	"se",
+}
+
+func main() {
+	metaFile := flag.String("meta", "", "metadata file that matches []subsheet format")
+	flag.Parse()
+	if *metaFile == "" {
+		must("-meta must be set")
+	}
+	data, err := ioutil.ReadFile(*metaFile)
+	must(err)
+	var sheet Sheet
+	err = yaml.Unmarshal(data, &sheet)
+	must(err)
+
+	var atlas Atlas
+	//create atlas
+	for _, subsheet := range sheet.Subsheets {
+		animationName := subsheet.Name
+		width := (subsheet.End.X - subsheet.Start.X)/ subsheet.Columns
+		height := (subsheet.End.Y - subsheet.Start.Y) / len(rows)
+		for row, direction := range rows {
+			y0 := subsheet.Start.Y + row * (height + sheet.RowSpacing)
+			for col := 0; col < subsheet.Columns; col++ {
+				frameName := fmt.Sprintf("%s.%s.%04d", animationName, direction, col)
+				x0 := subsheet.Start.X + col * width
+				box := Box{
+					X: x0,
+					Y: y0,
+					W: width,
+					H: height,
+				}
+				atlas.Frames = append(atlas.Frames, Frame{
+					Filename: frameName,
+					Box: box,
+				})
+			}
+		}
+	}
+	data, err = json.Marshal(atlas)
+	must(err)
+	fmt.Printf("%s", data)
+}
---
+++ sheetsplitter/types.go
@@ -0,0 +1,113 @@
+package main
+
+//custom stuff
+type Point struct {
+	X int `yaml:"x"`
+	Y int `yaml:"y"`
+}
+
+type Subsheet struct {
+	Name       string `yaml:"name"`
+	Start      Point `yaml:"start"`
+	End        Point `yaml:"end"`
+	Columns    int `yaml:"columns"`
+}
+
+type Sheet struct {
+	Subsheets []Subsheet `yaml:"subsheets"`
+	RowSpacing int `yaml:"row_spacing"`
+}
+
+
+//Atlas stuff
+type Atlas struct {
+	Frames []Frame `json:"frames"`
+}
+
+type Frame struct {
+	Filename string `json:"filename"`
+	Box Box `json:"frame"`
+	Rotated bool `json:"rotated"`
+	Trimmed bool `json:"trimmed"`
+	SpriteSourceSize struct {
+			 X int `json:"x"`
+			 Y int `json:"y"`
+			 W int `json:"w"`
+			 H int `json:"h"`
+		 } `json:"spriteSourceSize"`
+	SourceSize struct {
+			 W int `json:"w"`
+			 H int `json:"h"`
+		 } `json:"sourceSize"`
+}
+
+type Box struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+	W int `json:"w"`
+	H int `json:"h"`
+}
+
+type Anims struct {
+	Attack struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"attack"`
+	Idle struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"idle"`
+	Walk struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"walk"`
+	GetHit struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"get_hit"`
+	Die struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"die"`
+	Spell struct {
+		       S []string `json:"s"`
+		       Sw []string `json:"sw"`
+		       W []string `json:"w"`
+		       Nw []string `json:"nw"`
+		       N []string `json:"n"`
+		       Ne []string `json:"ne"`
+		       E []string `json:"e"`
+		       Se []string `json:"se"`
+	       } `json:"spell"`
+
+}
